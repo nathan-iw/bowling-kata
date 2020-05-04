98930a4e387bc3806f310934328bec6fee1fd9df
Alex <Alexander.Wallwork@infinityworks.com>
2020-05-04 14:52:54 +0100

    scores a spare as well as non spares

--- Game.py
+++ Game.py
@@ -1,10 +1,19 @@
 class Game: 
     def __init__(self):
-        self.score = 0
+        self.rolls = []
 
     def roll(self, pins):
-        self.score += pins
+        self.rolls.append(pins) 
 
     def get_score(self):
-        return self.score
-        
+        score = 0
+        i = 0
+        for frame in range(10):
+            if self.rolls[i] + self.rolls[i+1] == 10:
+                score += 10 + self.rolls[i+2]
+                i += 2
+            else:
+                score += self.rolls[i] + self.rolls[i+1]
+                i += 2
+        return score
+    
--- test/test_main.py
+++ test/test_main.py
@@ -20,8 +20,12 @@ class Test_Game(unittest.TestCase):
         self.roll_many(20, 1)
         self.assertEqual(20, self.g.get_score())
 
-
-
+    def test_one_spare(self):
+        self.g.roll(5)
+        self.g.roll(5)
+        self.g.roll(3)
+        self.roll_many(17,0)
+        self.assertEqual(16, self.g.get_score())
     
 if __name__ == '__main__':
     unittest.main()
