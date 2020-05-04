b58c7661184ceeb6f49fce3279bf4904bf87b0ba
Alex <Alexander.Wallwork@infinityworks.com>
2020-05-04 13:48:38 +0100

    passed first test ; v

---
+++ test/test_main.py
@@ -0,0 +1,11 @@
+import unittest 
+from Game import Game
+
+class Test_Game(unittest.TestCase):
+
+    def test_gutter_game(self):
+        g = Game()
+
+        
+if __name__ == '__main__':
+    unittest.main()
