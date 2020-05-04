3c507d3fddd7c74ebc64b822b3399b93b6abb39d
Alex <Alexander.Wallwork@infinityworks.com>
2020-05-04 14:06:12 +0100

    added and passed second test

--- Game.py
+++ Game.py
@@ -1,3 +1,10 @@
 class Game: 
-    pass
+    def __init__(self):
+        self.score = 0
 
+    def roll(self, pins):
+        self.score += pins
+
+    def get_score(self):
+        return self.score
+        
--- test/test_main.py
+++ test/test_main.py
@@ -5,7 +5,17 @@ class Test_Game(unittest.TestCase):
 
     def test_gutter_game(self):
         g = Game()
+        for i in range(20): 
+            g.roll(0)
+        self.assertEqual(0, g.get_score())
 
-        
+    
+    def test_all_one(self):
+        g = Game()
+        for i in range(20):
+            g.roll(1)
+        self.assertEqual(20, g.get_score())
+
+    
 if __name__ == '__main__':
     unittest.main()
